357d48c3b80ebf9a4316f757e11829880b96c44e
Marcus Crabtree <kmarcuscrabtree@gmail.com>
2020-03-18 20:49:15 -0400

    adding solution to HW

---
+++ sqljbc_members_queries_solutions.sql
@@ -0,0 +1,9 @@
+SELECT * FROM sqljbc_members.members;
+select * from members
+	where role = 'developer4';
+select * from members
+	order by last_name asc;
+select * from members
+	order by salary desc;
+ select * from members
+	where first_name like 's%';
